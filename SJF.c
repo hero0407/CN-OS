a5046f79bce45834fe274b61cbc8712606050ffb
hero0407 <mouryagoguri4@gmail.com>
2025-06-05 01:56:37 +0530

    Create SJF.c

---
+++ SJF.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+void main() 
+{
+    int pid[10], bt[10], wt[10], tat[10], n, twt = 0, ttat = 0;
+    int i, j, t;
+    float awt, atat;
+
+    printf("Enter no. of processes: ");
+    scanf("%d", &n);
+
+    printf("\nEnter burst times: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &bt[i]);
+
+    printf("\nEnter PID: ");
+    for (i = 0; i < n; i++)
+        scanf("%d", &pid[i]);
+
+    // Sort processes by burst time (SJF)
+    for (i = 0; i < n; i++) 
+    {
+        for (j = i + 1; j < n; j++) 
+        {
+            if (bt[i] > bt[j]) 
+            {
+                // Swap burst time
+                t = bt[i];
+                bt[i] = bt[j];
+                bt[j] = t;
+
+                // Swap PID
+                t = pid[i];
+                pid[i] = pid[j];
+                pid[j] = t;
+            }
+        }
+    }
+
+    wt[0] = 0;
+    tat[0] = bt[0];
+
+    for (i = 1; i < n; i++) 
+    {
+        wt[i] = tat[i - 1];
+        tat[i] = bt[i] + wt[i];
+    }
+
+    for (i = 0; i < n; i++) 
+    {
+        ttat += tat[i];
+        twt += wt[i];
+    }
+
+    printf("\nPID\tBT\tWT\tTAT");
+    for (i = 0; i < n; i++)
+        printf("\n%d\t%d\t%d\t%d", pid[i], bt[i], wt[i], tat[i]);
+
+    awt = (float)twt / n;
+    atat = (float)ttat / n;
+
+    printf("\nAvg. Waiting Time = %f", awt);
+    printf("\nAvg. Turn Around Time = %f", atat);
+}
